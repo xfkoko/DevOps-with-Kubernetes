8037c8a650d571d18a4dca78fd6de5b840effb77
xfkoko <kosti.korhonen@tuni.fi>
2022-06-22 10:45:05 +0300

    1.12 Done

---
+++ 1/1.12/app.js
@@ -0,0 +1,58 @@
+const http = require("http");
+var request = require("request");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3002;
+
+//const directory = path.join("/", "usr", "src", "app", "files");
+const directory = path.join(".");
+const filePath = path.join(directory, "date.txt");
+const imagePath = path.join(directory, "image.png");
+
+function downloadImage() {
+    var url = "https://picsum.photos/1200";
+    request.head(url, function(err, res, body) {
+        request(url).pipe(fs.createWriteStream(imagePath)).on("close", function(){console.log("Image req done.")});
+    });
+}
+
+function dateMaker(oldDate) {
+    var newDate = new Date();
+    var month = newDate.getUTCMonth() + 1;
+    var day = newDate.getUTCDate();
+    var year = newDate.getUTCFullYear();
+    finalDate = day + "/" + month + "/" + year;
+    return finalDate;
+}
+
+const server = http.createServer((req, res) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            return res.end("Failed to read file");
+        }
+        var currentDate = dateMaker(new Date());
+        if (content == currentDate) {
+            res.statusCode == 200;
+            res.setHeader("Content-Type", "image/png");
+            var img = fs.readFileSync(imagePath);
+            res.end(img, "binary");
+        } else {
+            fs.writeFile(filePath, currentDate, err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("Something written to date file")
+                }
+            });
+            downloadImage();
+            res.statusCode == 200;
+            res.setHeader("Content-Type", "image/png");
+            var img = fs.readFileSync(imagePath);
+            res.end(img, "binary");
+        }  
+    });
+});
+
+console.log("Server started in port " + port);
+server.listen(port);
